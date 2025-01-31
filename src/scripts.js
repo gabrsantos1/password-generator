2ce7fc5376662ca55f78efa60e91949dda765a9c
Gabriel Santos <gs2625020@gmail.com>
2025-01-30 21:18:24 -0300

    feat(pw): password generator working, missing copy message

--- src/scripts.js
+++ src/scripts.js
@@ -1,23 +1,43 @@
 function passwordGenerator(){
-    const upperLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-    const lowerLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    const number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    const symbol = ["!", "`", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "{", "|", "}", "~"];
+    
+    const upperLet = document.getElementById("upperLet").checked;
+    const lowerLet = document.getElementById("lowerLet").checked;
+    const number = document.getElementById("number").checked;
+    const symbol = document.getElementById("symbol").checked;
+
+    //const upperLetter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    //const lowerLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    //const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    //const symbols = ["!", "`", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "{", "|", "}", "~"];
+    const upperLetter = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+    const lowerLetter = ["abcdefghijklmnopqrstuvwxyz"];
+    const numbers = ["0123456789"];
+    const symbols = ["!`#$%&()*+,-./:;<=>?@[]^_{|}~"];
+
+    const size = document.getElementById("size").value;
+    
+    if(size <= 0){
+        alert("Selecione um tamanho válido para sua senha.");
+        return;
+    }
 
     let pwConfig = ""
 
     if(upperLet == true){
         pwConfig += upperLetter;
-    } else if (lowerLet == true){
+    }
+    if (lowerLet == true){
         pwConfig += lowerLetter;
-    } else if (number == true){
-        pwConfig += number;
-    } else if (symbol == true){
-        pwConfig += symbol;
+    }
+    if (number == true){
+        pwConfig += numbers;
+    }
+    if (symbol == true){
+        pwConfig += symbols;
     }
 
     if(pwConfig.length === 0){
-        alert("Selecione pelo menos uma especificação para sua senha.")
+        alert("Selecione pelo menos uma especificação para sua senha.");
     }
 
     let pwComplete = "";
@@ -26,6 +46,6 @@ function passwordGenerator(){
         pwComplete += pwConfig[randomizer];
     }
 
-    textContent = "Senha: " + pwComplete;
+    document.getElementById("pwOutput").value = "Senha: " + pwComplete;
 
 }
